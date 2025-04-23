d566081f6c5fc10ea5787c219d1abb2a0556a703
AzehFavour <140659018+AzehFavour@users.noreply.github.com>
2025-04-23 15:52:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Lord Naira',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Lord Naira Bot",
+  botOwner: 'Lord Naira', // Your name
+  ownerNumber: '237676327871', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
